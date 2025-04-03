1224ba7c9be468b6cd3cc6d414b428e22a69cc16
Portal do Agro <aliviajr12@gmail.com>
2025-04-03 19:23:50 -0300

    ola

--- src/data/Portais.js
+++ src/data/Portais.js
@@ -1,5 +1,6 @@
 export const Portais = [
     { id: 1, nome: "Bokashi", url: "https://portalagro.com.br" },
     { id: 2, nome: "Hortas", url: "https://portalagro.com.br/noticias" },
-    { id: 3, nome: "blabalda", url: "https://portalagro.com.br/noticias" },
+    { id: 3, nome: "Hortas 2", url: "https://portalagro.com.br/noticias" },
+    { id: 4, nome: "Hortas 3", url: "https://portalagro.com.br/noticias" },
 ]
---
+++ src/pages/Home/home.style.jsx
@@ -0,0 +1,1 @@
+export const Portais = style.div
