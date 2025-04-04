bec0ab56e86dce9056bed6fbf74c923e31e3166e
luiz-nd <lipeunaind13@gmail.com>
2025-04-03 21:09:17 -0300

    footer

---
+++ src/components/Footer/Footer.styles.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styled from 'styled-components';
+
+export const FooterContainer = styled.footer`
+    background-color: #2a5d20;
+    color: white;
+    text-align: center;
+    padding: 10px 0;
+    position: fixed;
+    width: 100%;
+    bottom: 0;
+`;
